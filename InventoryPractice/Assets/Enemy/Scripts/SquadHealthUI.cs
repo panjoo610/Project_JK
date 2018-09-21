740db45e2e1c2d7bf8d087f3a6620301b4207e73
정진영 <qufqlaud@gmail.com>
2018-09-21 15:38:06 +0900

    충돌해결 _ enemyScripts

--- InventoryPractice/Assets/Enemy/Scripts/SquadHealthUI.cs
+++ InventoryPractice/Assets/Enemy/Scripts/SquadHealthUI.cs
@@ -16,24 +16,10 @@ public class SquadHealthUI : MonoBehaviour
         Debug.Log(sq);
         cam = Camera.main.transform;
 
-        //uiXPosition = sq.transform.position.x;
-
-        //uiYPosition = sq.transform.position.y;
         ui = transform.GetChild(0).gameObject.transform;
     }
     void LateUpdate()
     {
-        //gameObject.transform.forward = new Vector3(cam.position.x, transform.rotation.y, -cam.position.z); ;// = new Vector3(cam.position.x, cam.position.y, -cam.position.z);
         ui.transform.position = Camera.main.WorldToScreenPoint(sq.transform.position);
     }
-    
-    
-
-
-    //void OnGUI()
-    //{
-
-    //    GUI.Box(new Rect(uiXPosition - (healthBarLength / 2), uiYPosition, healthBarLength, 20), curHealth + "/" + maxHealth);
-
-    //}
 }
