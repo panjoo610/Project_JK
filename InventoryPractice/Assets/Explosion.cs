3fd76419216dec02b28c3bab90bffb321f7c3edb
정진영 <qufqlaud@gmail.com>
2018-11-23 15:36:56 +0900

    4스테이지 수정

---
+++ InventoryPractice/Assets/Explosion.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+    public ParticleSystem Particle;
+    public SphereCollider[] colliders;
+    public float ExplosionRange;
+
+    public float DestroyTime;
+
+    float runtime;
+    // Use this for initialization
+    void Start () {
+        colliders = GetComponentsInChildren<SphereCollider>();
+        test();
+    }
+
+    private void Update()
+    {
+        //particleSystem
+        runtime += Time.deltaTime;
+        if (runtime >= 5f)
+        {
+            test();
+            DestroyTime -= Time.deltaTime;
+            if (DestroyTime <= 0f)
+            {
+                gameObject.SetActive(false);
+            } 
+        }
+
+    }
+
+    void test()
+    {
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            //colliders[i].radius = Mathf.Lerp(colliders[i].radius, ExplosionRange, 10f * Time.deltaTime);
+            //if (colliders[i].radius == ExplosionRange)
+            //{
+            //    colliders[i].enabled = false;
+            //}
+            colliders[i].enabled = true;
+        }
+    }
+
+}
