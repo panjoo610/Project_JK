146ec8f7f754d759a55f438e71f5513fc15d46a2
정진영 <qufqlaud@gmail.com>
2018-11-27 17:52:25 +0900

    집가기전

---
+++ InventoryPractice/Assets/ActionZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionZone : MonoBehaviour {
+    Collider collider;
+    public GameObject ActiveGameObject;
+
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<Collider>();
+	}
+	
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ActiveGameObject.SetActive(!ActiveGameObject.activeSelf); 
+        }
+    }
+
+
+}
